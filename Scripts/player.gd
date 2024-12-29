b1f6cccacb1148f884e7e075e446d6c2123684cb
pimogbu1 <pimogbu@gmail.com>
2024-12-29 17:56:50 +0300

    Add crosshair

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,19 +11,18 @@ var camera_input_direction := Vector2.ZERO
 
 @onready var camera_pivot: Node3D = %CameraPivot
 @onready var camera_3d: Camera3D = %Camera3D
-@onready var gun_barrel = $CameraPivot/Rifle/RayCast3D
-@onready var anim_player = $CameraPivot/Rifle/AnimationPlayer
 
 #stamina variables
 var SPRINT_SPEED = 8.0
 var WALK_SPEED = 5.0
 var stamina = 100
-
+#head movement variables
 const char_FREQ = 2.0
 const char_AMP = 0.08
 var t_char = 0.0
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _ready() -> void: #Start the game by capturing the mouse
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("six"):
@@ -32,13 +31,12 @@ func _input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE #now you cant use your mouse
 
 func _unhandled_input(event: InputEvent) -> void:
-			
+	# Set camera_input_direction
 	var is_camera_motion := (event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED)
 	if is_camera_motion:
 		camera_input_direction = event.screen_relative * mouse_sensitivity
 		
 func _physics_process(delta: float) -> void:
-
 	# Handle sprint 
 	if Input.is_action_pressed("sprint") and stamina >= 75:
 		move_speed = SPRINT_SPEED
@@ -47,14 +45,15 @@ func _physics_process(delta: float) -> void:
 		move_speed = WALK_SPEED
 		stamina += 2.0 * delta
 		stamina = clamp(stamina, 0, 100)
-
-
+	# Set camera angle by using camera_input_direction
 	camera_pivot.rotation.x -= camera_input_direction.y * delta
 	camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -PI / 3.0, PI / 1.5) #Limit for Vision Rotate
 	camera_pivot.rotation.y -= camera_input_direction.x * delta 
 	
-	camera_input_direction = Vector2.ZERO
+	camera_input_direction = Vector2.ZERO 
 	
+	if Input.is_action_just_pressed("left_click"):
+		pass
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -76,6 +75,7 @@ func _process(delta: float) -> void:
 	# Head char 
 	t_char += delta * velocity.length() * float(is_on_floor())
 	camera_3d.transform.origin = _headchar(t_char)
+
 	move_and_slide()
 func _headchar(time) -> Vector3:
 	var pos = Vector3.ZERO
