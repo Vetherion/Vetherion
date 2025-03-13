84159a5b6b69e705b902c43e3d93c4b98006bf5b
Siyabend Akdemir <siyabend1818@gmail.com>
2025-03-13 21:15:55 +0300

    Drop button function added but buggy. #1

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -3,6 +3,12 @@ extends HBoxContainer
 var selected_item: String
 @export var player: Node3D
 @onready var camera = %Camera3D
+var level: Node3D
+@export var sniper0_scene: PackedScene
+const A = 30
+
+func _ready() -> void:
+	level = player.get_parent()
 
 # Equip button
 func _on_equip_pressed() -> void:
@@ -19,5 +25,9 @@ func _on_inequip_pressed() -> void:
 
 # Drop button
 func _on_drop_pressed() -> void:
-	#TODO: Make a drop function
-	pass
+	var item = sniper0_scene.instantiate()
+	level.add_child(item)
+	#item.position = Vector3(0.5,0.5,0.5)
+	#print(player.get_node("CameraPivot").rotation)
+	#item.position = Vector3(player.position.x + 2, player.position.y, player.position.z)
+	item.position = Vector3(player.position.x + 0.05*A, player.position.y, player.position.z + sqrt(2*(A^2) - 2*(A^2)*cos(player.get_node("CameraPivot").rotation.y)))
